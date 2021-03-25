a0e73350a8b078b011167d83322db35a0c6aab95
Mike Ona <ona_mike@yahoo.com>
2021-03-25 16:49:22 -0700

    Added files

--- script.js
+++ script.js
@@ -7,18 +7,45 @@ const gameBoard = (board,index) =>{
         console.log(board);
     };
 
-    const setBoard = (index) => {
-        board[index] = 1;
+    const setBoard = (index) => { //add a check winning combo function
+        
+        if(player=="player1"){
+            if(board[index]==null){
+                board[index] = "X";
+            }
+        }
+        else if(player=="player2"){
+            if(board[index]==null){
+                board[index] = "O";
+            }
+        } 
+        
     };
 
     const clearBoard = () => {
         board = new Array(9);
     };
+
+    const setPlayer1 = () => {
+        player = "player1";
+        gridMarker = "X";
+        gridFilledClass = "grid-item-filled-X";
+    };
+
+    const setPlayer2 = () => {
+        player = "player2";
+        gridMarker = "O";
+        gridFilledClass = "grid-item-filled-O";
+    };
     
-    return {getBoard, setBoard, clearBoard}
+    return {getBoard, setBoard, clearBoard,setPlayer1,setPlayer2}
 };
 
 let board = new Array(9);
+let player = "player1";
+let gridMarker = "X";
+let gridFilledClass = "grid-item-filled-X";
+
 const boardObject = gameBoard(board);
 
 
@@ -27,12 +54,26 @@ $('.grid-item').click(function() {
     boardObject.getBoard();
 });
 
+$('#clear-btn').click(function() { //need to clear CSS
+    boardObject.clearBoard(); 
+});
+
+$('#player1-btn').click(function() {
+    boardObject.setPlayer1(); 
+});
+
+$('#player2-btn').click(function() {
+    boardObject.setPlayer2();
+});
+
 let grids = document.getElementsByClassName("grid-item");
 for(x=0;x<grids.length;x++){
-    grids[x].addEventListener('click', e => e.target.classList.add('grid-item-filled'));
-    grids[x].addEventListener('click', e => e.target.innerHTML = "X");
+    grids[x].addEventListener('click', e => e.target.classList.add(gridFilledClass));
+    grids[x].addEventListener('click', e => e.target.innerHTML = gridMarker);
 };
     
+
+
 
 //Add conditionals
 //Add Computer AI
